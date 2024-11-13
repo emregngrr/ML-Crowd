8707840e8b4c70215cadd5464e683cc7e3fe80a4
Emre Güngör <emre.gungor.1109@gmail.com>
2024-11-13 22:39:15 +0300

    Clone Create and Line Up Complete!
    Started making obstacles.

--- My project/Assets/Scripts/CrowdController.cs
+++ My project/Assets/Scripts/CrowdController.cs
@@ -1,27 +1,25 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CrowdController : MonoBehaviour
 {
-
     public float MoveSpeed = 5;
     public float SteerSpeed = 180;
     public float CloneSpeed = 5;
     public int Gap = 10;
-    //Döngü
-    private int temp = 1;
-
 
     public GameObject BodyPrefab;
 
     private List<GameObject> Clones = new List<GameObject>();
     private List<Vector3> PositionsHistory = new List<Vector3>();
-    private Vector3 moveDirection = new Vector3(0,0,0);
 
     void Start()
     {
+        AddClone();
+        AddClone();
+        AddClone();
+        AddClone();
+        AddClone();
         AddClone();
         AddClone();
         AddClone();
@@ -39,49 +37,40 @@ public class CrowdController : MonoBehaviour
         foreach (var clone in Clones)
         {
             Vector3 point = PositionsHistory[Mathf.Clamp(index * Gap, 0, PositionsHistory.Count - 1)];
+            Vector3 moveDirection;
 
-            switch (temp)
+            switch (index % 3)
             {
+                case 0:
+                    // Sað arka sýra
+                    moveDirection = point - clone.transform.position + new Vector3(3, 0, -3);
+                    break;
                 case 1:
-                    //Sað arka sýra
-                    Debug.Log("Case 1:"+moveDirection);
-                    moveDirection = point - clone.transform.position + new Vector3(3, 0, -1);
+                    // Orta arka sýra
+                    moveDirection = point - clone.transform.position + new Vector3(0, 0, -3);
                     break;
                 case 2:
-                    //Orta arka sýra
-                    Debug.Log("Case 2:" + moveDirection);
-                    moveDirection = point - clone.transform.position + new Vector3(0, 0, -1);
+                    // Sol arka sýra
+                    moveDirection = point - clone.transform.position + new Vector3(-3, 0, -3);
                     break;
-                case 3:
-                    //Sol arka sýra
-                    Debug.Log("Case 3:" + moveDirection);
-                    moveDirection = point - clone.transform.position + new Vector3(-3, 0, -1);
+                default:
+                    moveDirection = point - clone.transform.position;
                     break;
-
             }
-            /*
-            //Sað arka sýra
-            Vector3 moveDirection = point - clone.transform.position + new Vector3(3,0,-1);
-            //Orta arka sýra
-            Vector3 moveDirection = point - clone.transform.position + new Vector3(3, 0, -1);
-            //Sol arka sýra
-            Vector3 moveDirection = point - clone.transform.position + new Vector3(3, 0, -1);
-            */
-
+            
             clone.transform.position += moveDirection * CloneSpeed * Time.deltaTime;
 
-            // Rotate body towards the point along the snakes path
-            clone.transform.LookAt(point);
-            temp++;
-            if (temp > 3)
-                temp = 1;
+            // Rotate body towards the point along the path
+            //clone.transform.LookAt(point);
+            clone.transform.rotation = Quaternion.identity;
+
             index++;
         }
     }
 
     private void AddClone()
     {
-         GameObject clone = Instantiate(BodyPrefab);
-         Clones.Add(clone);
+        GameObject clone = Instantiate(BodyPrefab, new Vector3(0,0,-2),Quaternion.identity);
+        Clones.Add(clone);
     }
 }
